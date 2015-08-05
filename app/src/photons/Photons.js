d951aea6daa809800fbe087e389c3cbac11ce1e7
Eric Dalquist <eric.dalquist@gmail.com>
2015-08-05 07:33:14 -0700

    Cleanup

--- app/src/photons/Photons.js
+++
@@ -1,8 +0,0 @@
-(function(){
-  'use strict';
-
-  // Prepare the 'photons' module for subsequent registration of controllers and delegates
-  angular.module('photons', [ 'ngMaterial' ]);
-
-
-})();
